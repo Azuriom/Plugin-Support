69ae06917b90be30bc0f2bbcfd36fdd0f4b2a3cb
MrMicky <git@mrmicky.fr>
2022-02-20 13:53:36 +0100

    Update default design

--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -3,66 +3,66 @@
 @section('title', $ticket->subject)
 
 @section('content')
-    <div class="container content">
-        <h1>{{ $ticket->subject }}</h1>
+    <h1>{{ $ticket->subject }}</h1>
 
-        <div class="card shadow-sm mb-3">
-            <div class="card-body">
+    <div class="card mb-3">
+        <div class="card-body">
              <span class="badge bg-{{ $ticket->isClosed() ? 'danger' : 'success' }}">
                  {{ $ticket->statusMessage() }}
              </span>
-                @lang('support::messages.tickets.info', ['author' => e($ticket->author->name), 'category' => e($ticket->category->name), 'date' => format_date($ticket->created_at)])
-            </div>
+            @lang('support::messages.tickets.info', ['author' => e($ticket->author->name), 'category' => e($ticket->category->name), 'date' => format_date($ticket->created_at)])
         </div>
+    </div>
 
-        @foreach($ticket->comments as $comment)
-            <div class="card shadow-sm mb-3">
-                <div class="card-header @if($ticket->author->is($comment->author)) text-info @else text-primary @endif">
-                    @lang('messages.comments.author', ['user' => e($comment->author->name), 'date' => format_date($comment->created_at, true)])
-                </div>
-                <div class="card-body d-flex">
-                    <img class="flex-shrink-0 me-3 rounded" src="{{ $comment->author->getAvatar() }}" alt="{{ $comment->author->name }}" height="55">
-                    <div class="flex-grow-1">
-                        <div class="content-body">
-                            {{ $comment->parseContent() }}
-                        </div>
+    @foreach($ticket->comments as $comment)
+        <div class="card mb-3">
+            <div class="card-body d-flex">
+                <img class="flex-shrink-0 me-3 rounded" src="{{ $comment->author->getAvatar() }}" alt="{{ $comment->author->name }}" height="55">
+                <div class="flex-grow-1">
+                    <div class="content-body">
+                        <p class="mb-1 small">
+                            @lang('messages.comments.author', ['user' => e($comment->author->name), 'date' => format_date($comment->created_at, true)])
+                        </p>
+
+                        {{ $comment->parseContent() }}
                     </div>
                 </div>
             </div>
-        @endforeach
+        </div>
+    @endforeach
 
-        <div class="card shadow-sm mb-3">
+    @if($ticket->isClosed())
+        <div class="alert alert-info">
+            <i class="fas fa-info-circle"></i> {{ trans('support::messages.tickets.closed') }}
+        </div>
+    @else
+        <div class="card mb-3">
             <div class="card-body">
-                @if($ticket->isClosed())
-                    <p class="text-danger">{{ trans('support::messages.tickets.closed') }}</p>
-                @else
-                    <form action="{{ route('support.tickets.comments.store', $ticket) }}" method="POST" class="mb-2">
-                        @csrf
+                <form action="{{ route('support.tickets.comments.store', $ticket) }}" method="POST" class="mb-2">
+                    @csrf
 
-                        <div class="mb-3">
-                            <label class="form-label" for="content">{{ trans('support::messages.fields.comment') }}</label>
-                            <textarea class="form-control @error('content') is-invalid @enderror" id="content" name="content" rows="4" required>{{ old('content') }}</textarea>
-                        </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="content">{{ trans('support::messages.fields.comment') }}</label>
+                        <textarea class="form-control @error('content') is-invalid @enderror" id="content" name="content" rows="4" required>{{ old('content') }}</textarea>
+                    </div>
 
-                        @error('content')
-                        <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
-                        @enderror
+                    @error('content')
+                    <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+                    @enderror
 
-                        <button type="submit" class="btn btn-primary">
-                            {{ trans('messages.actions.comment') }}
-                        </button>
-                    </form>
+                    <button type="submit" class="btn btn-primary">
+                        <i class="fas fa-comment"></i> {{ trans('messages.actions.comment') }}
+                    </button>
+                </form>
 
-                    <form action="{{ route('support.tickets.close', $ticket) }}" method="POST">
-                        @csrf
+                <form action="{{ route('support.tickets.close', $ticket) }}" method="POST">
+                    @csrf
 
-                        <button type="submit" class="btn btn-danger">
-                            {{ trans('support::messages.actions.close') }}
-                        </button>
-                    </form>
-                @endif
-
+                    <button type="submit" class="btn btn-danger">
+                        <i class="fas fa-ban"></i> {{ trans('support::messages.actions.close') }}
+                    </button>
+                </form>
             </div>
         </div>
-    </div>
+    @endif
 @endsection
