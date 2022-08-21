470e6c9e668b62d4d0044ef0ecdd0496f07a80a6
MODcraft <admin@ncgs.top>
2022-08-21 15:56:07 +0000

    Added Chinese Simplified Translation

---
+++ resources/lang/zh-CN/admin.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'title' => '支持',
+
+    'categories' => [
+        'title' => '类别管理',
+        'edit' => '编辑类别 #:category',
+        'create' => '创建类别',
+
+	'delete_empty' => '此类别下存在工单，还不能被删除.',
+    ],
+
+    'tickets' => [
+        'title' => '工单',
+        'show' => '工单 #:ticket - :name',
+        'create' => '创建工单',
+
+        'open' => '打开工单',
+    ],
+
+    'permissions' => [
+        'tickets' => '查看和管理支持票',
+        'categories' => '查看和管理支持类别',
+    ],
+
+    'settings' => [
+        'title' => '支持设置',
+        'webhook' => 'Discord Webhook URL',
+        'webhook_info' => '当用户创建工单或添加评论时，它将在此 Webhook 上创建一个通知。留空则禁用',
+    ],
+
+    'logs' => [
+        'tickets' => [
+            'closed' => '已关闭工单 #:id',
+        ],
+    ],
+];
---
+++ resources/lang/zh-CN/messages.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+    'title' => '支持',
+
+    'fields' => [
+        'subject' => '主题',
+        'category' => '类别',
+        'ticket' => '工单',
+	'comment' => '评论',
+    ],
+
+    'actions' => [
+        'create' => '创建一个新的工单',
+        'reopen' => '重新打开',
+        'close' => '关闭',
+    ],
+
+    'state' => [
+        'open' => '开启',
+        'closed' => '关闭',
+    ],
+
+    'tickets' => [
+        'closed' => '此工单已关闭',
+
+        'open' => '创建工单',
+
+        'notification' => '您的支持工单上的新回复',
+
+        'info' => '<strong>:author</strong> 于 :date 创建了类别为 <strong>:category</strong> 的工单',
+    ],
+
+    'webhook' => [
+        'ticket' => '支持上的新工单',
+        'comment' => '关于支持的新评论',
+        'closed' => '工单已关闭',
+    ],
+
+    'mails' => [
+        'comment' => [
+            'subject' => '您的工单上的新回复.',
+            'message' => '您好 :user，您的工单 #:id 已由 :author 回复.',
+            'view' => '查看工单',
+        ],
+    ],
+];
